5262008c25a6cf63551d45b720f5a2dfccae1034
Eric Thompson <thompsoneric3259@gmail.com>
2023-10-26 22:53:32 -0400

    Add mousedown event listener to grid

--- javascript.js
+++ javascript.js
@@ -6,15 +6,22 @@ function createGrid(size) {
         let ithDiv = document.createElement("div");
         ithDiv.className = "row";
         for (let j = 1; j <= size; j++) {
-            ithDiv.appendChild(document.createElement("div"));
+            let pixel = document.createElement("div");
+            pixel.className = "square"
+            ithDiv.appendChild(pixel);
         }
         grid.appendChild(ithDiv);
     }
     gridContainer.appendChild(grid);
+    grid.addEventListener("mousedown", squareMouseDown;
 }
 
 function removeGrid() {
     const gridContainer = document.querySelector(".grid-container");
     const grid = document.querySelector(".grid");
     gridContainer.removeChild(grid);
+}
+
+function squareMouseDown(e) {
+    console.log(e.target)
 }
